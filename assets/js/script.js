ac7db20838e1c4854da7073e3ffb3cb538226f21
Amadewe <dewevre.amandine@gmail.com>
2019-01-22 16:56:33 +0100

    Fin de l'exercice 4

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function sup(){
+document.getElementById('lastname').value="";
+document.getElementById('firstname').value="";
+document.getElementById('city').value="";
+}
+// value="" permet de supprimer les valeurs 
